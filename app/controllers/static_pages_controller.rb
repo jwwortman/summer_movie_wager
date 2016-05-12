df95630359259106c4baf4dbebfceffa0e58c3a4
jwwortman <jacob.w.wortman@gmail.com>
2016-05-12 17:00:40 -0400

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
 
   def rules
   end
+
+  def about
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Summer Movie Wager"
   end
 
   test "should get rules" do
     get :rules
     assert_response :success
+    assert_select "title", "Rules | Summer Movie Wager"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Summer Movie Wager"
+  end
 end
